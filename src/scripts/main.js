4735c941264c55987b7c9f1953a1daa32ba1da1d
Jackson Henderson <jbhhenderson@gmail.com>
2023-01-12 13:33:54 -0600

    Added seed scripts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,14 @@
+import { createPlan } from "./plan.js";
+
 console.log("Welcome to the main module")
 
+const yearlyPlan = createPlan()
+
+for (const plan of yearlyPlan) {
+    console.log(plan)
+}
+
+// import { createAsparagus } from "./seeds/asparagus.js"
+
+// const asparagusSeed = createAsparagus()
+// console.log(asparagusSeed)
